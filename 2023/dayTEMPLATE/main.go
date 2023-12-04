764de5a29e8d9a5ed144ae20c721d690b3c05587
Conor McNamara <conortm@gmail.com>
2023-12-04 16:52:34 +0000

    Add template for days

---
+++ 2023/dayTEMPLATE/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/conortm/aoc/2023/utils"
+)
+
+func partOne() {
+	utils.Intro("PART 1")
+}
+
+func partTwo() {
+	utils.Intro("PART 2")
+}
+
+func main() {
+	partOne()
+	partTwo()
+}
